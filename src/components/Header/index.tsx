20ad8084521c0260aa84d1e7231d5d17d66b5d38
Rafael Ramos <rafaelramosdev@gmail.com>
2022-01-28 23:46:57 -0300

    refactor: refactor component in javascript to typescript and changed structure from class component to function component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,36 +1,32 @@
-import { Component } from 'react';
-
 import { FiPlusSquare } from 'react-icons/fi';
 
 import Logo from '../../assets/logo.svg';
 
 import { Container } from './styles';
 
-class Header extends Component {
-  render() {
-    const { openModal } = this.props;
+interface HeaderProps {
+  openModal: () => void;
+}
 
-    return (
-      <Container>
-        <header>
-          <img src={Logo} alt="GoRestaurant" />
-          <nav>
-            <div>
-              <button
-                type="button"
-                onClick={openModal}
-              >
-                <div className="text">Novo Prato</div>
-                <div className="icon">
-                  <FiPlusSquare size={24} />
-                </div>
-              </button>
-            </div>
-          </nav>
-        </header>
-      </Container>
-    )
-  }
-};
-
-export default Header;
+export function Header(props: HeaderProps) {
+  return (
+    <Container>
+      <header>
+        <img src={Logo} alt="GoRestaurant" />
+        <nav>
+          <div>
+            <button
+              type="button"
+              onClick={props.openModal}
+            >
+              <div className="text">Novo Prato</div>
+              <div className="icon">
+                <FiPlusSquare size={24} />
+              </div>
+            </button>
+          </div>
+        </nav>
+      </header>
+    </Container>
+  )
+}
